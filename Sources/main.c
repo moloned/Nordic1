473bbccbc5b4180bc3ded666a04b56bcf4e5ca19
Liam G <Liam@lg-mac-mini.gateway.2wire.net>
2014-05-03 21:35:26 -0700

    Boot to 16MHZ clock and GPIO

--- Sources/main.c
+++ Sources/main.c
@@ -7,12 +7,19 @@
 
 int main(void)
 {
+	HFCLKSTART=0xffff;	// Start the external 16MHz crystal oscillator
+
+	PINCNF6=0x1;		// Set P0.06 to regular output mode
+	volatile unsigned int count=0;
 
 
-
-
-    while (1);
-
+	while (1)
+	{
+		for (count=0;count<10000;count++);
+		OUTSET=0x40;	// Set P0.06 high
+		for (count=0;count<10000;count++);
+		OUTCLR=0x40;	// Set P0.06 low
+	}
 
 }
 
